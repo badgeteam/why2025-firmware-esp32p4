1ca998d8b4c9e1123d48f7a514763a857a64e738
Hakuamesan <hakuame@gmail.com>
2024-05-29 20:11:49 +0100

    initial files

---
+++ README.md
@@ -0,0 +1,5 @@
+# WHY 2025 Badge Firmware
+
+This is a template for now. It will be updated soon.
+
+
---
+++ main/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <esp_app_desc.h>
+#include <esp_err.h>
+#include <esp_log.h>
+#include <esp_ota_ops.h>
+#include <esp_system.h>
+
+
+void display_version(){
+    const esp_app_desc_t* app_description = esp_app_get_description();
+    printf("BADGE.TEAM %s launcher firmware v%s\n", app_description->project_name, app_description->version);
+}
+
+
+void app_main(void)
+{
+    display_version();
+}
