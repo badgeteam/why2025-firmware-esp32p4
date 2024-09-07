946d084c1837013256330d251e79a042bc71a82d
RobotMan2412 <julian@scheffers.net>
2024-09-07 13:57:19 +0200

    Barebones app launching menu

--- main/menus/apps.c
+++ main/menus/apps.c
@@ -24,10 +24,7 @@ void menu_apps_open() {
         pgui_elem_t *button = pgui_new_button(NULL, start_app_cb);
         pgui_child_append(grid, button);
         pgui_set_userdata(button, app_list + i);
-        pgui_enable_flags(
-            button,
-            PGUI_FLAG_FIX_WIDTH | PGUI_FLAG_FIX_HEIGHT | PGUI_FLAG_NOBORDER | PGUI_FLAG_NOPADDING
-        );
+        pgui_enable_flags(button, PGUI_FLAG_FIX_WIDTH | PGUI_FLAG_FIX_HEIGHT | PGUI_FLAG_NOPADDING);
         pgui_set_size2(button, 400, 38);
         pgui_elem_t *name = pgui_new_text(app_list[i].name ?: app_list[i].id);
         pgui_child_append(button, name);
