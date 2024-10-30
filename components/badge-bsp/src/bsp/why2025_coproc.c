954c0f65f4b2584fb8225d8649273a3ffbc0f563
Orange-Murker <23561952+Orange-Murker@users.noreply.github.com>
2024-10-30 22:26:08 +0100

    feat(coproc): use the published tanmatsu component

--- components/badge-bsp/src/bsp/why2025_coproc.c
+++ components/badge-bsp/src/bsp/why2025_coproc.c
@@ -4,9 +4,11 @@
 #include "bsp/why2025_coproc.h"
 
 #include "bsp.h"
+#include "driver/i2c_master.h"
+#include "hardware/why2025.h"
+#include "tanmatsu_coprocessor.h"
 
 #include <driver/gpio.h>
-#include <driver/i2c.h>
 #include <driver/sdmmc_host.h>
 #include <esp_log.h>
 #include <freertos/FreeRTOS.h>
@@ -17,11 +19,8 @@
 
 static char const TAG[] = "coprocessor";
 
-static SemaphoreHandle_t ch32_int_semaphore;
-static SemaphoreHandle_t ch32_semaphore;
-static TaskHandle_t      ch32_thread_handle;
-static void              ch32_thread();
-static void              ch32_isr(void *arg);
+SemaphoreHandle_t             i2c_concurrency_semaphore = NULL;
+tanmatsu_coprocessor_handle_t coprocessor_handle        = NULL;
 static uint32_t          ch32_input_dev_id;
 static uint32_t          ch32_input_dev_ep;
 static sdmmc_card_t      c6_card;
@@ -32,221 +31,113 @@ static sdmmc_host_t      sdmmc_host = SDMMC_HOST_DEFAULT();
 
 /* ==== platform-specific functions ==== */
 
-// Get the C6 version.
+// Get the CH32 version.
 esp_err_t bsp_ch32_version(uint16_t *ver) {
-    if (!why2025_enable_i2cint)
-        return ESP_ERR_TIMEOUT;
-    xSemaphoreTake(ch32_semaphore, portMAX_DELAY);
-    esp_err_t res = i2c_master_write_read_device(
-        BSP_I2CINT_NUM,
-        BSP_CH32_ADDR,
-        (uint8_t const[]){0x00},
-        1,
-        (void *)ver,
-        2,
-        pdMS_TO_TICKS(100)
-    );
-    xSemaphoreGive(ch32_semaphore);
-    return res;
+    return tanmatsu_coprocessor_get_firmware_version(coprocessor_handle, ver);
+}
+
+void coprocessor_keyboard_callback(
+    tanmatsu_coprocessor_handle_t handle, tanmatsu_coprocessor_keys_t *prev_keys, tanmatsu_coprocessor_keys_t *keys
+) {
+    uint8_t *buttons      = keys->raw;
+    uint8_t *prev_buttons = prev_keys->raw;
+
+    // Fire button changed events.
+    for (int row = 0; row < 9; row++) {
+        for (int col = 0; col < 8; col++) {
+            if (((buttons[row] & ~prev_buttons[row]) >> col) & 1) {
+                bsp_raw_button_pressed(ch32_input_dev_id, ch32_input_dev_ep, row * 8 + col);
+            } else if (((~buttons[row] & prev_buttons[row]) >> col) & 1) {
+                bsp_raw_button_released(ch32_input_dev_id, ch32_input_dev_ep, row * 8 + col);
+            }
+        }
+    }
+}
+
+void coprocessor_input_callback(
+    tanmatsu_coprocessor_handle_t  handle,
+    tanmatsu_coprocessor_inputs_t *prev_inputs,
+    tanmatsu_coprocessor_inputs_t *inputs
+) {
+    if ((inputs->sd_card_detect) != (prev_inputs->sd_card_detect)) {
+        // SD card detect changed
+        ESP_LOGW(TAG, "SD card %s", (inputs->sd_card_detect) ? "inserted" : "removed");
+    }
+
+    if ((inputs->headphone_detect) != (prev_inputs->headphone_detect)) {
+        // Headphone detect changed
+        ESP_LOGW(TAG, "Audio jack %s", (inputs->headphone_detect) ? "inserted" : "removed");
+    }
 }
 
 // Initialise the co-processor drivers.
 esp_err_t bsp_why2025_coproc_init() {
-    // Initialise the CH32V203 driver.
-    ch32_int_semaphore = xSemaphoreCreateBinary();
-    ch32_semaphore     = xSemaphoreCreateBinary();
-    xSemaphoreGive(ch32_semaphore);
-    if (ch32_int_semaphore == NULL || ch32_semaphore == NULL) {
-        return ESP_ERR_NO_MEM;
-    }
-    if (xTaskCreate(ch32_thread, "CH32 driver", 2048, NULL, 0, &ch32_thread_handle) != pdTRUE) {
-        return ESP_ERR_NO_MEM;
-    }
+    esp_err_t res;
 
-    esp_err_t res = gpio_isr_handler_add(BSP_CH32_IRQ_PIN, ch32_isr, NULL);
-    if (res != ESP_OK) {
-        return res;
-    }
+    i2c_concurrency_semaphore = xSemaphoreCreateBinary();
+    xSemaphoreGive(i2c_concurrency_semaphore);
 
-    gpio_config_t sao_cfg = {
-        .pin_bit_mask = BIT64(BSP_CH32_IRQ_PIN),
-        .mode         = GPIO_MODE_INPUT,
-        .pull_up_en   = false,
-        .pull_down_en = false,
-        .intr_type    = GPIO_INTR_NEGEDGE,
+    i2c_master_bus_config_t i2c_master_config = {
+        .clk_source                   = I2C_CLK_SRC_DEFAULT,
+        .i2c_port                     = BSP_I2CINT_NUM,
+        .scl_io_num                   = BSP_I2CINT_SCL_PIN,
+        .sda_io_num                   = BSP_I2CINT_SDA_PIN,
+        .glitch_ignore_cnt            = 7,
+        .flags.enable_internal_pullup = true,
     };
-    res = gpio_config(&sao_cfg);
+
+    i2c_master_bus_handle_t i2c_bus_handle = NULL;
+
+    res = i2c_new_master_bus(&i2c_master_config, &i2c_bus_handle);
     if (res != ESP_OK) {
         return res;
     }
 
-    xSemaphoreGive(ch32_int_semaphore); // Workaround for firmware bug in CH32V203 firmware, will be fixed soon
+    tanmatsu_coprocessor_config_t coprocessor_config = {
+        .int_io_num            = BSP_CH32_IRQ_PIN,
+        .i2c_bus               = i2c_bus_handle,
+        .i2c_address           = BSP_CH32_ADDR,
+        .concurrency_semaphore = i2c_concurrency_semaphore,
+        .on_keyboard_change    = coprocessor_keyboard_callback,
+        .on_input_change       = coprocessor_input_callback,
+    };
+
+    res = tanmatsu_coprocessor_initialize(&coprocessor_config, &coprocessor_handle);
+    if (res != ESP_OK) {
+        return res;
+    }
 
     return res;
-}
-
-
-
-// CH32V203 driver thread.
-static void ch32_thread() {
-    uint8_t prev_buttons[9] = {0};
-    uint8_t buttons[9];
-    uint8_t prev_inputs = 0;
-    uint8_t inputs;
-    ESP_LOGI(TAG, "CH32V203 thread started");
-    while (true) {
-        // Wait for an interrupt to be noticed.
-        xSemaphoreTake(ch32_int_semaphore, portMAX_DELAY);
-
-        // When an interrupt occurs, read buttons from I²C.
-        xSemaphoreTake(ch32_semaphore, portMAX_DELAY);
-        esp_err_t res = i2c_master_write_read_device(
-            BSP_I2CINT_NUM,
-            BSP_CH32_ADDR,
-            (uint8_t[]){2}, // I2C_REG_KEYBOARD_0
-            1,
-            buttons,
-            9,
-            pdMS_TO_TICKS(50)
-        );
-
-        if (res != ESP_OK) {
-            xSemaphoreGive(ch32_semaphore);
-            continue;
-        }
-
-        // Read input status
-        res = i2c_master_write_read_device(
-            BSP_I2CINT_NUM,
-            BSP_CH32_ADDR,
-            (uint8_t[]){15}, // I2C_REG_INPUT
-            1,
-            &inputs,
-            1,
-            pdMS_TO_TICKS(50)
-        );
-
-        xSemaphoreGive(ch32_semaphore);
-        if (res != ESP_OK) {
-            continue;
-        }
-
-        // Fire button changed events.
-        for (int row = 0; row < 9; row++) {
-            for (int col = 0; col < 8; col++) {
-                if (((buttons[row] & ~prev_buttons[row]) >> col) & 1) {
-                    bsp_raw_button_pressed(ch32_input_dev_id, ch32_input_dev_ep, row * 8 + col);
-                } else if (((~buttons[row] & prev_buttons[row]) >> col) & 1) {
-                    bsp_raw_button_released(ch32_input_dev_id, ch32_input_dev_ep, row * 8 + col);
-                }
-            }
-        }
-
-        if ((inputs & 1) != (prev_inputs & 1)) {
-            // SD card detect changed
-            ESP_LOGW(TAG, "SD card %s", (inputs & 1) ? "inserted" : "removed");
-        }
-
-        if ((inputs & 2) != (prev_inputs & 2)) {
-            // Headphone detect changed
-            ESP_LOGW(TAG, "Audio jack %s", (inputs & 2) ? "inserted" : "removed");
-        }
-
-        prev_inputs = inputs;
-
-        memcpy(prev_buttons, buttons, sizeof(buttons));
-    }
-}
-
-// CH32V203 interrupt handler.
-IRAM_ATTR static void ch32_isr(void *arg) {
-    (void)arg;
-    xSemaphoreGiveFromISR(ch32_int_semaphore, NULL);
-    portYIELD_FROM_ISR();
 }
 
 // Set the display backlight value.
-esp_err_t ch32_set_display_backlight(uint16_t value) {
-    xSemaphoreTake(ch32_semaphore, portMAX_DELAY);
-    uint8_t buffer[3];
-    buffer[0] = 11; // I2C_REG_DISPLAY_BACKLIGHT_0
-    buffer[1] = value & 0xFF;
-    buffer[2] = value >> 8;
-    esp_err_t res
-        = i2c_master_write_to_device(BSP_I2CINT_NUM, BSP_CH32_ADDR, buffer, sizeof(buffer), pdMS_TO_TICKS(100));
-    xSemaphoreGive(ch32_semaphore);
-    return res;
+esp_err_t ch32_set_display_backlight(uint8_t value) {
+    return tanmatsu_coprocessor_set_display_backlight(coprocessor_handle, value);
 }
 
 // Set the keyboard backlight value.
-esp_err_t ch32_set_keyboard_backlight(uint16_t value) {
-    xSemaphoreTake(ch32_semaphore, portMAX_DELAY);
-    uint8_t buffer[3];
-    buffer[0] = 13; // I2C_REG_KEYBOARD_BACKLIGHT_0
-    buffer[1] = value & 0xFF;
-    buffer[2] = value >> 8;
-    esp_err_t res
-        = i2c_master_write_to_device(BSP_I2CINT_NUM, BSP_CH32_ADDR, buffer, sizeof(buffer), pdMS_TO_TICKS(100));
-    xSemaphoreGive(ch32_semaphore);
-    return res;
+esp_err_t ch32_set_keyboard_backlight(uint8_t value) {
+    return tanmatsu_coprocessor_set_keyboard_backlight(coprocessor_handle, value);
 }
 
 // Get the display backlight value.
-esp_err_t ch32_get_display_backlight(uint16_t *value) {
-    if (!value)
-        return ESP_OK;
-    xSemaphoreTake(ch32_semaphore, portMAX_DELAY);
-    esp_err_t res = i2c_master_write_read_device(
-        BSP_I2CINT_NUM,
-        BSP_CH32_ADDR,
-        (uint8_t const[]){11}, // I2C_REG_DISPLAY_BACKLIGHT_0
-        1,
-        (void *)value,
-        2,
-        pdMS_TO_TICKS(100)
-    );
-    xSemaphoreGive(ch32_semaphore);
-    return res;
+esp_err_t ch32_get_display_backlight(uint8_t *value) {
+    return tanmatsu_coprocessor_get_display_backlight(coprocessor_handle, value);
 }
 
 // Get the keyboard backlight value.
-esp_err_t ch32_get_keyboard_backlight(uint16_t *value) {
-    if (!value)
-        return ESP_OK;
-    xSemaphoreTake(ch32_semaphore, portMAX_DELAY);
-    esp_err_t res = i2c_master_write_read_device(
-        BSP_I2CINT_NUM,
-        BSP_CH32_ADDR,
-        (uint8_t const[]){13}, // I2C_REG_KEYBOARD_BACKLIGHT_0
-        1,
-        (void *)value,
-        2,
-        pdMS_TO_TICKS(100)
-    );
-    xSemaphoreGive(ch32_semaphore);
-    return res;
+esp_err_t ch32_get_keyboard_backlight(uint8_t *value) {
+    return tanmatsu_coprocessor_get_keyboard_backlight(coprocessor_handle, value);
 }
 
 // Enable/disable the audio amplifier.
 esp_err_t bsp_amplifier_control(bool enable) {
-    xSemaphoreTake(ch32_semaphore, portMAX_DELAY);
-    uint8_t buffer[2];
-    buffer[0]     = 16; // I2C_REG_AMPLIFIER_ENABLE
-    buffer[1]     = enable ? 1 : 0;
-    esp_err_t res = i2c_master_write_to_device(BSP_I2CINT_NUM, BSP_CH32_ADDR, buffer, sizeof(buffer), portMAX_DELAY);
-    xSemaphoreGive(ch32_semaphore);
-    return res;
+    return tanmatsu_coprocessor_set_amplifier_enable(coprocessor_handle, enable);
 }
 
 // Enable/disable the ESP32-C6 via RESET and BOOT pins.
 esp_err_t bsp_c6_control(bool enable, bool boot) {
-    xSemaphoreTake(ch32_semaphore, portMAX_DELAY);
-    uint8_t buffer[2];
-    buffer[0]     = 17; // I2C_REG_RADIO_CONTROL
-    buffer[1]     = (enable & 1) | ((boot & 1) << 1);
-    esp_err_t res = i2c_master_write_to_device(BSP_I2CINT_NUM, BSP_CH32_ADDR, buffer, sizeof(buffer), portMAX_DELAY);
-    xSemaphoreGive(ch32_semaphore);
-    return res;
+    return tanmatsu_coprocessor_set_radio_state(coprocessor_handle, (enable & 1) | ((boot & 1) << 1));
 }
 
 // Initialize the ESP32-C6 after it was (re-)enabled.
@@ -287,22 +178,13 @@ bool bsp_input_why2025ch32_init(bsp_device_t *dev, uint8_t endpoint) {
 
 // Get current input value by raw input number.
 bool bsp_input_why2025ch32_get_raw(bsp_device_t *dev, uint8_t endpoint, uint16_t raw_input) {
-    uint8_t buttons = 0;
+    tanmatsu_coprocessor_keys_t keys;
+    tanmatsu_coprocessor_get_keyboard_keys(coprocessor_handle, &keys);
 
-    // Read buttons from I²C.
-    xSemaphoreTake(ch32_semaphore, portMAX_DELAY);
-    i2c_master_write_read_device(
-        BSP_I2CINT_NUM,
-        BSP_CH32_ADDR,
-        (uint8_t[]){2 + raw_input / 8}, // I2C_REG_KEYBOARD_0
-        1,
-        &buttons,
-        1,
-        pdMS_TO_TICKS(50)
-    );
-    xSemaphoreGive(ch32_semaphore);
+		uint16_t row = raw_input / 8;
+		uint16_t col = raw_input % 8;
 
-    return (buttons >> (raw_input & 7)) & 1;
+    return (keys.raw[row] >> col) & 1;
 }
 
 
